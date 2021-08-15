adc6c2d32c744d543d2da02730bff8be1aba9fd4
massenandev <massenandev@gmail.com>
2021-08-15 17:56:34 -0300

    init: setting initial artifacts

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import { categoriesRoutes } from './routes/categories.routes'
+
+const app = express()
+
+app.use(express.json())
+
+app.use(categoriesRoutes)
+
+app.listen(3333, () => console.log('Server is running'))
