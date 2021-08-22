8b1d75eb2ecd22fe143ecd520a637c5546afac2b
massenandev <massenandev@gmail.com>
2021-08-22 10:55:37 -0300

    feat(importCategories): load categories from an file from request

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,9 +1,65 @@
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+import csvParse from 'csv-parse'
+// file stream
+import fs from 'fs'
 
+interface IImportCategory {
+  name: string
+  description: string
+}
 
 class ImportCategoryUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
 
-  execute(file: any){
-    console.log(file)
+  
+  // responsável somente por fazer a leitura das categorias
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path)
+    
+      const categories: IImportCategory[] = []
+
+      //o csv ja entende que a virgula é o delimitador. o csvParse é responsável por ler linha por linha do arquivo. 
+      // isso é possível porque a cada pedaço lido, estamos passando o pipe
+      const parseFile = csvParse()
+      // pipe pega o que tá sendo lido do stream e dentro dele, ele joga o que foi lido pra o lugar determinado
+      // a cada chunck lido, o pipe vai enviar pra onde queremos que seja enviado
+      stream.pipe(parseFile)
+
+      parseFile.on('data', async (line) => {
+        // ["name", "description"]
+        // desestruturação para que cada posição vai colocar na variável
+        const [name, description] = line
+        categories.push({
+          name,
+          description
+        })
+      })
+      .on('end', () => {
+        resolve(categories)
+      })
+      .on('error', (err) => {
+        reject(err)
+      })
+    })
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    // com o async/await, ele vai esperar a finalização para inserir no banco de dados
+    const categories = await this.loadCategories(file)
+    // o map percorre o array e da pra fazer umas manipulações
+    categories.map(async (category) => {
+      const { name, description } = category
+
+      const existCategory = this.categoriesRepository.findByName(name)
+
+      if(!existCategory){
+        this.categoriesRepository.create({
+          name, 
+          description
+        })
+      }
+    }) 
   }
 }
 
--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,7 +1,10 @@
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
-const importCategoryUseCase = new ImportCategoryUseCase()
+// está como singleton para ter somente uma instância
+const categoriesRepository = CategoriesRepository.getInstance()
+const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository)
 const importCategoryController = new ImportCategoryController(importCategoryUseCase)
 
 export { importCategoryController }
