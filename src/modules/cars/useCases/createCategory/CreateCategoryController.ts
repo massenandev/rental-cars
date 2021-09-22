1eaeec44d912b1e3e4160581e44369cc841122ac
massenandev <massenandev@gmail.com>
2021-09-21 21:53:36 -0300

    fix(category): missing letter

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
-import { CreateCategoyUseCase } from "./CreateCategoryUseCase";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 
 class CreateCategoryController {
 
-  constructor(private createCategoryUseCase: CreateCategoyUseCase) {}
+  constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
 
   handle(request: Request, response: Response): Response {
     const { name, description } = request.body
