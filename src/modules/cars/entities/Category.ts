efcdc383d77321f894ae914c1a39a3a4d116ac16
massenandev <massenandev@gmail.com>
2021-09-23 19:48:58 -0300

    feat(database): add categories table

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,9 +1,20 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn } from 'typeorm'
 import { v4 as uuidV4 } from 'uuid'
 
+@Entity("categories")
 class Category {
+  
+  
+  @PrimaryColumn()
   id?: string
+  
+  @Column()
   name: string
+  
+  @Column()
   description: string
+  
+  @CreateDateColumn()
   created_at: Date
 
   constructor(){
