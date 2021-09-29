ff66615364ce890f2f9e493c8e0c9dd0baa42735
massenandev <massenandev@gmail.com>
2021-09-29 10:17:39 -0300

    feat(users): create user

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,24 @@
+import { inject } from "tsyringe";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+class CreateUserUseCase {
+  
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ name, username, email, password, driver_license }: ICreateUserDTO): Promise<void> {
+
+    await this.usersRepository.create({
+      name, 
+      username, 
+      email, 
+      password, 
+      driver_license
+    })
+  }
+}
+
+export { CreateUserUseCase }
