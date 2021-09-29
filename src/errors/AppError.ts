7df058ef51a2c2d892332d1c8051d9056380619d
massenandev <massenandev@gmail.com>
2021-09-29 16:52:02 -0300

    feat(errors): customize errors

--- src/errors/AppError.ts
+++ src/errors/AppError.ts
@@ -4,6 +4,7 @@ export class AppError {
   public readonly statusCode: number
 
   // default será erro 400
+  // com o erro customizado, da pra controlar melhor o status do erro e a msg
   constructor(message: string, statusCode = 400) {
     this.message = message
     this.statusCode = statusCode
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
-import express from 'express'
+import "reflect-metadata"
+import express, { NextFunction, Request, Response } from 'express'
+import "express-async-errors"
 import swaggerUi from 'swagger-ui-express'
 
 import './database'
 
 import './shared/container'
 
+import { AppError } from './errors/AppError';
 import { router } from './routes'
 import swaggerFile from './swagger.json'
 
@@ -15,6 +18,20 @@ app.use(express.json())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(router)
+
+//middleware de erro
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: `Internal server error - ${err.message}`
+  })
+})
 
 
 app.listen(3333, () => console.log('Server is running'))
