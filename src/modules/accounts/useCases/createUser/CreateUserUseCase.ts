945431c1620bf6eb7e83941d82891391ba055558
massenandev <massenandev@gmail.com>
2021-09-29 10:31:38 -0300

    fix(users): add injectable annotation

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,9 +1,9 @@
-import { inject } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
+@injectable()
 class CreateUserUseCase {
-  
   constructor(
     @inject("UsersRepository")
     private usersRepository: IUsersRepository
