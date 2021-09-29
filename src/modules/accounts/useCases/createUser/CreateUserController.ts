1298185854af91761df6483296891a1a012a92c0
massenandev <massenandev@gmail.com>
2021-09-29 10:32:05 -0300

    feat(users): add routes, index and controller to create a new user

---
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,24 @@
+import { container } from 'tsyringe';
+import { Request, Response } from "express";
+import { CreateUserUseCase } from './CreateUserUseCase';
+
+class CreateUserController {
+
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { name, username, email, password, driver_license } = request.body
+    
+    const createUserUseCase = container.resolve(CreateUserUseCase)
+
+    await createUserUseCase.execute({
+      name, 
+      username,
+      email,
+      password,
+      driver_license
+    })
+
+    return response.status(201).send()
+  }
+}
+
+export { CreateUserController }
