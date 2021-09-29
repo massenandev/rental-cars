08d615a5f680909ea560e66bfd02d8ff61785bb4
massenandev <massenandev@gmail.com>
2021-09-29 12:00:30 -0300

    feat(authenticate): add jwt for auth purposes

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,58 @@
+import { compare } from "bcryptjs"
+import { sign } from "jsonwebtoken"
+import { inject, injectable } from "tsyringe"
+
+import { IUsersRepository } from "../../repositories/IUsersRepository"
+
+interface IRequest {
+  email: string
+  password: string
+}
+
+interface IResponse {
+  user: {
+    name: string,
+    email: string
+  }
+  token: string
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+    //se o user existe
+    if(!user) {
+      throw new Error("Email or password incorrect")
+    }
+    // se a senha correta
+    const passwordMatch = await compare(password, user.password)
+
+    if(!passwordMatch) {
+      throw new Error("Email or password incorrect")
+    }
+
+    const token = sign({}, "cffc1b3daada1c85a7b326f2a6e41386", {
+      subject: user.id,
+      expiresIn: "1d"
+    })
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name, 
+        email: user.email
+      }
+    }
+
+    //gerar o jwt
+    return tokenReturn
+  }
+}
+
+export { AuthenticateUserUseCase }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { authenticateRoutes } from './authenticate.routes'
 import { categoriesRoutes } from './categories.routes'
 import { specificationsRoutes } from './specifications.routes'
 import { usersRoutes } from './users.routes'
@@ -9,5 +10,7 @@ const router = Router()
 router.use('/categories', categoriesRoutes)
 router.use('/specifications', specificationsRoutes)
 router.use('/users', usersRoutes)
+router.use(authenticateRoutes) // dessa forma, não tem o path
+
 
 export { router }
