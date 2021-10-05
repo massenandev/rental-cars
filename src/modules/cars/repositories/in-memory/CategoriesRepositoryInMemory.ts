9acd87ebaa93a0073ecb26c6b4396f9ac5488897
massenandev <massenandev@gmail.com>
2021-10-04 22:13:58 -0300

    test(users): add test to create an user

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,28 @@
+import { Category } from "../../entities/Category";
+import { ICategoriesRepository, ICreateCategoryDTO } from "../ICategoriesRepository";
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+
+  categories: Category[] = []
+
+  async findByName(name: string): Promise<Category> {
+    return this.categories.find((category) => category.name === name)
+  }
+
+  async list(): Promise<Category[]> {
+    return this.categories
+  }
+  
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const category = new Category()
+
+    Object.assign(category, {
+      name,
+      description
+    })
+
+    this.categories.push(category)
+  }
+}
+
+export { CategoriesRepositoryInMemory }
