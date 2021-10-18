d30864809ccb0d1f169f3d5d244c0fc7a464965b
massenandev <massenandev@gmail.com>
2021-10-17 21:05:31 -0300

    refactor(entities): move to a specific folder

--- src/modules/cars/infra/typeorm/repositories/CategoriesRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CategoriesRepository.ts
@@ -1,5 +1,5 @@
 import { getRepository, Repository } from "typeorm"
-import { Category } from "../Category"
+import { Category } from "../entities/Category"
 import { ICategoriesRepository, ICreateCategoryDTO } from "../../../repositories/ICategoriesRepository"
 class CategoriesRepository implements ICategoriesRepository {
 
