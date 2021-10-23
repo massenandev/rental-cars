748c7b29a491b28eb272d5451b722083ba44f82f
massenandev <massenandev@gmail.com>
2021-10-23 20:37:51 -0300

    fix(database): add correct host

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,6 +1,6 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm"
 
-export default async(host = "database"): Promise<Connection> => {
+export default async(host = "database_ignite"): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions()
 
   return createConnection(
